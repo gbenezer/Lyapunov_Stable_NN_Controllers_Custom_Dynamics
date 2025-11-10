c45e57eef5e9ca9063cad8b65ae8b90113ff85e7
Gil Benezer <gil.benezer@gmail.com>
2025-11-10 08:35:14 -0500

    minor changes to suffixes for wandb tracking

--- slurm_job_script.sh
+++ slurm_job_script.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 #SBATCH --partition=gpu
 #SBATCH --nodes=1 # number of nodes ()
-#SBATCH --gres=gpu:v100-pcie:1 # type of GPU
-#SBATCH --time=04:00:00
-#SBATCH --job-name=quadrotor2d_output_08Nov2025_V100_PCIe_set_1
+#SBATCH --gres=gpu:v100-sxm2:1 # type of GPU
+#SBATCH --time=02:00:00
+#SBATCH --job-name=pendulum_state_symbolic_v100-sxm2
 #SBATCH --mem=8GB
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --output=output/benezerg/quadrotor2d_output/job_logs/quadrotor2d_output_08Nov2025_V100_PCIe_set_1.%j.out
-#SBATCH --error=output/benezerg/quadrotor2d_output/job_logs/quadrotor2d_output_08Nov2025_V100_PCIe_set_1.%j.err
+#SBATCH --output=output/benezerg/pendulum_state/job_logs/pendulum_state_symbolic_v100-sxm2.%j.out
+#SBATCH --error=output/benezerg/pendulum_state/job_logs/pendulum_state_symbolic_v100-sxm2.%j.err
 
 export PYTHONPATH="${PYTHONPATH}:$(pwd):$(pwd)/alpha-beta-CROWN:$(pwd)/alpha-beta-CROWN/complete_verifier"
-for i in {1..6};
-do
-    python examples/quadrotor2d_output_training.py user=benezerg_quadrotor2d_output_training
-done
+python examples/pendulum_state_training_symbolic.py user=benezerg_pendulum_state_training
