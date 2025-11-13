78818e19874235c773ad94edf1258cdc0e0fc5e4
Fanghsin Li <fangxinli0122@gmail.com>
2025-11-13 16:14:40 -0500

    set up job submission for output for fanghsin

---
+++ slurm_job_script_sxm2_fanghsin.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --nodes=1 # number of nodes ()
+#SBATCH --gres=gpu:v100-sxm2:1 # type of GPU
+#SBATCH --time=02:00:00
+#SBATCH --job-name=quadrotor2d_output_v100-sxm2_train_controller
+#SBATCH --mem=8GB
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --output=output/fanghsinli/quadrotor2d_output/job_logs/quadrotor2d_output_v100-sxm2_train_controller.%j.out
+#SBATCH --error=output/fanghsinli/quadrotor2d_output/job_logs/quadrotor2d_output_v100-sxm2_train_controller.%j.err
+
+export PYTHONPATH="${PYTHONPATH}:$(pwd):$(pwd)/alpha-beta-CROWN:$(pwd)/alpha-beta-CROWN/complete_verifier"
+python examples/quadrotor2d_output_training.py user=fanghsinli_quadrotor2d_output_training_sxm2
