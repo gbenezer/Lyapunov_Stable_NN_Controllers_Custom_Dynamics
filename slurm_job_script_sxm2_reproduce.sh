0bfec5d26b11fbd2e50e5a4fd8cbd97837e18d72
Gil Benezer <gil.benezer@gmail.com>
2025-11-15 18:29:07 -0500

    changed back to hardcoded version

--- slurm_job_script_sxm2_reproduce.sh
+++ slurm_job_script_sxm2_reproduce.sh
@@ -2,13 +2,13 @@
 #SBATCH --partition=gpu
 #SBATCH --nodes=1 # number of nodes ()
 #SBATCH --gres=gpu:v100-sxm2:1 # type of GPU
-#SBATCH --time=04:00:00
-#SBATCH --job-name=pendulum_state_v100-sxm2_reproduce_symbolic
+#SBATCH --time=02:00:00
+#SBATCH --job-name=pendulum_state_v100-sxm2_reproduce
 #SBATCH --mem=8GB
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --output=output/benezerg/pendulum_state/job_logs/pendulum_state_v100-sxm2_reproduce_symbolic.%j.out
-#SBATCH --error=output/benezerg/pendulum_state/job_logs/pendulum_state_v100-sxm2_reproduce_symbolic.%j.err
+#SBATCH --output=output/benezerg/pendulum_state/job_logs/pendulum_state_v100-sxm2_reproduce.%j.out
+#SBATCH --error=output/benezerg/pendulum_state/job_logs/pendulum_state_v100-sxm2_reproduce.%j.err
 
 export PYTHONPATH="${PYTHONPATH}:$(pwd):$(pwd)/alpha-beta-CROWN:$(pwd)/alpha-beta-CROWN/complete_verifier"
-python examples/pendulum_state_training_symbolic.py --config-name pendulum_state_training_reproduce_symbolic
+python examples/pendulum_state_training.py --config-name pendulum_state_training_reproduce
