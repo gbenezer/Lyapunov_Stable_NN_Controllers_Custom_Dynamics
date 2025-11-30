423236e71e44a3ad3067b98992833993673bb0d9
Gil Benezer <gil.benezer@gmail.com>
2025-11-30 14:43:17 -0500

    added scripts for the two novel systems

---
+++ slurm_job_script_sxm2_pvtol.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --nodes=1 # number of nodes ()
+#SBATCH --gres=gpu:v100-sxm2:1 # type of GPU
+#SBATCH --time=08:00:00
+#SBATCH --job-name=pvtol_state_v100-sxm2_symbolic
+#SBATCH --mem=16GB
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --output=output/benezerg/pvtol_state/job_logs/pvtol_state_v100-sxm2_symbolic.%j.out
+#SBATCH --error=output/benezerg/pvtol_state/job_logs/pvtol_state_v100-sxm2_symbolic.%j.err
+
+# prior log files need to be moved from pendulum_output
+export PYTHONPATH="${PYTHONPATH}:$(pwd):$(pwd)/alpha-beta-CROWN:$(pwd)/alpha-beta-CROWN/complete_verifier"
+python examples/pvtol_state_training_symbolic.py
